d28460426347b0b4e637f1cd4bb68f4eae2bcaa3
Seggan <seggan21@gmail.com>
2024-04-18 13:12:08 -0400

    Start

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,20 +7,43 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
+val gdxVersion: String by project
+val ktxVersion: String by project
 
+dependencies {
+    api(kotlin("stdlib"))
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+
+    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:$gdxVersion")
+    implementation("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-desktop")
+    implementation("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-desktop")
+    api("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
+    api("com.badlogicgames.gdx:gdx:$gdxVersion")
+    api("io.github.libktx:ktx-app:$ktxVersion")
+    api("io.github.libktx:ktx-assets:$ktxVersion")
+    api("io.github.libktx:ktx-async:$ktxVersion")
+    api("io.github.libktx:ktx-collections:$ktxVersion")
+    api("io.github.libktx:ktx-freetype-async:$ktxVersion")
+    api("io.github.libktx:ktx-freetype:$ktxVersion")
+    api("io.github.libktx:ktx-graphics:$ktxVersion")
 }
+
+sourceSets.main.get().resources.srcDir(File(rootDir, "assets"))
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
 application {
-    mainClass.set("io.github.seggan.swgv.AppKt")
+    mainClass = "io.github.seggan.swgv.AppKt"
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+tasks.named<JavaExec>("run") {
+    workingDir = file("assets")
 }
--- app/src/main/kotlin/io.github.seggan.swgv/App.kt
+++ app/src/main/kotlin/io.github.seggan.swgv/App.kt
@@ -1,5 +1,11 @@
 package io.github.seggan.swgv
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
 fun main() {
-    println("Hello, World!")
+    Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("SpigotWorldGenVis")
+        setWindowedMode(640, 480)
+    })
 }
